e7caf658f3105994c54215e2167eab2a81406394
mohammad <m.ghasempr@gmail.com>
2024-03-07 13:59:27 +0100

    Add support for processing multiple input files

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 print("Importing libraries...")
+import os
 import sys
 import time
+import glob
 import argparse
+from pathlib import Path
 
 print("Libraries imported successfully.\n\n")
+
+
+def check_existence(input_args):
+    files_list = []
+    if input_args.dir:
+        files_list = [file for file in glob.glob('{}/*.yuv'.format(input_args.dir))]
+    elif input_args.input:
+        if Path(input_args.input).is_file():
+            files_list = [input_args.input]
+    else:
+        return files_list, False
+
+    return files_list, True
+
 
 def print_custom_help():
     print("EVCA:    Enhanced Video Complexity Analyzer v1.0")
@@ -13,6 +30,7 @@ def print_custom_help():
     print("-m /--method              Feature extraction method. Default is EVCA. [VCA, EVCA, SITI] ")
     print("-fi/--filter              Edge detection filter. Default is Sobel filter. [sobel, canny] ")
     print("-i /--input               Raw YUV input file name.")
+    print("-d /--dir                 The directory of batch files.")
     print("-r /--resolution          Set the resolution [w]x[h]. Default is 1920x1080.")
     print("-b /--block_size          Set the block size. Default is 32. [8, 16, 32]")
     print("-f /--frames              Maximum number of frames for features extraction.")
@@ -25,7 +43,8 @@ def print_custom_help():
     
 def get_parser_arguments() -> argparse.Namespace:
         parser = argparse.ArgumentParser(add_help=False,)
-        parser.add_argument('-i',  '--input', type=str, default='./0001.yuv')
+        parser.add_argument('-i',  '--input', type=str)
+        parser.add_argument('-d',  '--dir', type=str)
         parser.add_argument('-m',  '--method', type=str, default='EVCA')
         parser.add_argument('-r',  '--resolution', type=str, default='1920x1080')
         parser.add_argument('-b',  '--block_size', type=int, default='32')
@@ -53,19 +72,25 @@ def main():
         from libs.SITI import SITI
         # Your main script logic goes here
         print("EVCA: Enhanced Video Complexity Analyzer v1.0.")
-        print("Start to extract features...")
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        t1 = time.time()
-        if args.method == 'EVCA':
-            EVCA(args,device)
-        elif args.method == 'VCA':
-            EVCA(args,device)
-        elif args.method == 'SITI':
-            SITI(args,device)
+
+        # Check input file(s) existence.
+        input_list, success = check_existence(args)
+        if success:
+            print("Start to extract features...")
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            t1 = time.time()
+            if args.method == 'EVCA':
+                EVCA(args,input_list,device)
+            elif args.method == 'VCA':
+                EVCA(args,input_list,device)
+            elif args.method == 'SITI':
+                SITI(args,input_list,device)
+            else:
+                print('Unsupported method.')
+            t2 = time.time()
+            print(f'Feature extraction completed in {t2-t1:.2f} seconds.')
         else:
-            print('Unsupported method.')
-        t2 = time.time()
-        print(f'Feature extraction completed in {t2-t1:.2f} seconds.')
+            print('Input file or directory is not specified.')
         
 if __name__ == "__main__":
     main()
