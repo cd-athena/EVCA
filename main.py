c9e81cea9df15689cb8e3da8b0f758db0ef9aef8
emanuele-artioli <emanuele.artioli@aau.at>
2024-06-24 13:24:53 +0200

    Revert "Revert "Merge pull request #6 from m-ghasempr/main""
    This reverts commit 1745332390b97d1b875de610defd7d29908be07e.

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def print_custom_help():
     print("-i /--input               Raw YUV input file name.")
     print("-d /--directory           Directory to multiple yuv files.")
     print("-r /--resolution          Set the resolution [w]x[h]. Default is 1920x1080.")
-    print("-b /--block_size          Set the block size. Default is 32. [8, 16, 32]")
+    print("-b /--block_size          Set the block size. Default is 32 and must be a multiple of 4.")
     print("-f /--frames              Maximum number of frames for features extraction.")
     print("-p /--pix_fmt             yuv format. Default is yuv420. [yuv420, yuv444] ")
     print("-s /--sample_rate         Frame subsampling. Default is 1 ")
@@ -41,32 +41,34 @@ def print_custom_help():
     print("-bi/--block_info          Write block level features into a csv. Default is disabled")
     print("-pi/--plot_info           Plot per frame features. Default is disabled")
     print("-dp/--dpi                 Image quality of the saved output. Default is 100.")
-    
+
+
 def get_parser_arguments() -> argparse.Namespace:
-        parser = argparse.ArgumentParser(add_help=False,)
-        parser.add_argument('-i',  '--input', type=str)
-        parser.add_argument('-d',  '--dir', type=str)
-        parser.add_argument('-m',  '--method', type=str, default='EVCA')
-        parser.add_argument('-t',  '--transform', type=str, default='DCT')
-        parser.add_argument('-r',  '--resolution', type=str, default='1920x1080')
-        parser.add_argument('-b',  '--block_size', type=int, default='32')
-        parser.add_argument('-f',  '--frames', type=int, default='30')
-        parser.add_argument('-c',  '--csv', type=str, default='./csv/test.csv')
-        parser.add_argument('-p',  '--pix_fmt', type=str, default='yuv420')
-        parser.add_argument('-s',  '--sample_rate', type=int, default='1') 
-        parser.add_argument('-bi', '--block_info', type=int, default='0')
-        parser.add_argument('-pi', '--plot_info', type=int, default='0')
-        parser.add_argument('-dp', '--dpi', type=int, default='100')
-        parser.add_argument('-fi', '--filter', type=str, default='sobel')
-        
-        return parser.parse_args()
-    
+    parser = argparse.ArgumentParser(add_help=False, )
+    parser.add_argument('-i', '--input', type=str, default='test.yuv')
+    parser.add_argument('-d', '--dir', type=str)
+    parser.add_argument('-m', '--method', type=str, default='EVCA')
+    parser.add_argument('-t', '--transform', type=str, default='DCT')
+    parser.add_argument('-r', '--resolution', type=str, default='3840x2160')
+    parser.add_argument('-b', '--block_size', type=int, default='32')
+    parser.add_argument('-f', '--frames', type=int, default='1')
+    parser.add_argument('-c', '--csv', type=str, default='./csv/test.csv')
+    parser.add_argument('-p', '--pix_fmt', type=str, default='yuv420')
+    parser.add_argument('-s', '--sample_rate', type=int, default='1')
+    parser.add_argument('-bi', '--block_info', type=int, default='0')
+    parser.add_argument('-pi', '--plot_info', type=int, default='0')
+    parser.add_argument('-dp', '--dpi', type=int, default='100')
+    parser.add_argument('-fi', '--filter', type=str, default='sobel')
+
+    return parser.parse_args()
+
+
 def main():
     # Check if the help option is explicitly provided
     if '-h' in sys.argv or '--help' in sys.argv:
         print_custom_help()
     else:
-        
+
         args = get_parser_arguments()
 
         import torch
@@ -82,17 +84,18 @@ def main():
             device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
             t1 = time.time()
             if args.method == 'EVCA':
-                EVCA(args,input_list,device)
+                EVCA(args, input_list, device)
             elif args.method == 'VCA':
-                EVCA(args,input_list,device)
+                EVCA(args, input_list, device)
             elif args.method == 'SITI':
-                SITI(args,input_list,device)
+                SITI(args, input_list, device)
             else:
                 print('Unsupported method.')
             t2 = time.time()
-            print(f'Feature extraction completed in {t2-t1:.2f} seconds.')
+            print(f'Feature extraction completed in {t2 - t1:.2f} seconds.')
         else:
             print('Input file or directory is not specified.')
-        
+
+
 if __name__ == "__main__":
     main()
